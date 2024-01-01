fd53cb132c127cfb99ed2c88eaadae6e9a807d59
ghozali1223 <ghozalipane@gmail.com>
2024-01-01 14:01:14 +0700

    Main

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -10,108 +10,108 @@ use Illuminate\Support\Facades\Storage;
 
 class BukuController extends Controller
 {
-    public function index()
-    {
-        $buku = Book::all();
-        return view('buku', ['buku' => $buku]);
-    }
+ public function index()
+ {
+  $buku = Book::all();
+  return view('buku', ['buku' => $buku]);
+ }
 
-    public function add_buku()
-    {
-        $dataKtgr = Category::all();
-        return view('add-buku', ['dataKtgr' => $dataKtgr]);
-    }
+ public function add_buku()
+ {
+  $dataKtgr = Category::all();
+  return view('add-buku', ['dataKtgr' => $dataKtgr]);
+ }
 
-    public function proses_add_buku(Request $request)
-    {
-        $request->validate([
-            'kode_buku' => 'unique:books', 'min:6', 'max:11',
-            'judul' => 'min:6', 'max:100',
-            'image' => 'image|file|max:2048|mimes:jpg,png'
-        ]);
+ public function proses_add_buku(Request $request)
+ {
+  $request->validate([
+   'kode_buku' => 'unique:books', 'min:6', 'max:11',
+   'judul' => 'min:6', 'max:100',
+   'image' => 'image|file|max:2048|mimes:jpg,png'
+  ]);
 
-        $newGbr = '';
+  $newGbr = '';
 
-        if ($request->file('image')) {
-            $eksGbr = $request->file('image')->getClientOriginalExtension();
-            $newGbr = $request->judul . '-' . now()->timestamp . '.' . $eksGbr;
-            $request->file('image')->storeAs('upload', $newGbr);
-        }
+  if ($request->file('image')) {
+   $eksGbr = $request->file('image')->getClientOriginalExtension();
+   $newGbr = $request->judul . '-' . now()->timestamp . '.' . $eksGbr;
+   $request->file('image')->storeAs('upload', $newGbr);
+  }
 
-        $request['cover'] = $newGbr;
-        $dataProses = Book::create($request->all());
-        $dataProses->categories()->sync($request->categories);
-        emotify('success', 'your data was successfully created');
-        return redirect('/buku');
-    }
+  $request['cover'] = $newGbr;
+  $dataProses = Book::create($request->all());
+  $dataProses->categories()->sync($request->categories);
+  emotify('success', 'your data was successfully created');
+  return redirect('/buku')->with('status', 'Berhasil tambah data judul: ' . $dataProses->judul);
+ }
 
-    public function edit_buku($slug)
-    {
-        $dataBuku = Book::where('slug', $slug)->first();
-        $dataKtgr = Category::all();
-        return view('edit-buku', ['dataBuku' => $dataBuku, 'dataKtgr' => $dataKtgr]);
-    }
+ public function edit_buku($slug)
+ {
+  $dataBuku = Book::where('slug', $slug)->first();
+  $dataKtgr = Category::all();
+  return view('edit-buku', ['dataBuku' => $dataBuku, 'dataKtgr' => $dataKtgr]);
+ }
 
-    public function buku_update(Request $request, $slug)
-    {
-        $request->validate([
-            'kode_buku' => 'required', 'min:6', 'max:11',
-            'judul' => 'min:6', 'max:100',
-            'cover' => 'image|file|max:2048|mimes:jpg,png'
-        ]);
-        // update image
-        if ($request->file('image')) {
-            $eksGbr = $request->file('image')->getClientOriginalExtension();
-            $newGbr = $request->judul . '-' . now()->timestamp . '.' . $eksGbr;
-            $request->file('image')->storeAs('upload', $newGbr);
-            $request['cover'] = $newGbr;
-        }
+ public function buku_update(Request $request, $slug)
+ {
+  $request->validate([
+   'kode_buku' => 'required', 'min:6', 'max:11',
+   'judul' => 'min:6', 'max:100',
+   'cover' => 'image|file|max:2048|mimes:jpg,png'
+  ]);
+  // update image
+  if ($request->file('image')) {
+   $eksGbr = $request->file('image')->getClientOriginalExtension();
+   $newGbr = $request->judul . '-' . now()->timestamp . '.' . $eksGbr;
+   $request->file('image')->storeAs('upload', $newGbr);
+   $request['cover'] = $newGbr;
+  }
 
-        // update slug
-        $request['slug'] = null;
-        $dataBuku = Book::where('slug', $slug)->first();
-        $dataBuku->update($request->all());
+  // update slug
+  $request['slug'] = null;
+  $dataBuku = Book::where('slug', $slug)->first();
+  $dataBuku->update($request->all());
 
-        //db transaction(update categories)
-        if ($request->categories) {
-            $dataBuku->categories()->sync($request->categories);
-        }
+  //db transaction(update categories)
+  if ($request->categories) {
+   $dataBuku->categories()->sync($request->categories);
+  }
 
-        return redirect('/buku')->with('status', 'Berhasil update data : ' . $dataBuku->judul);
-    }
+  return redirect('/buku')->with('status', 'Berhasil update data : ' . $dataBuku->judul);
+ }
 
-    public function delete_buku($slug)
-    {
-        $dataDelete = Book::where('slug', $slug)->first();
-        return view('buku-del', ['dataDelete' => $dataDelete]);
-    }
+ public function delete_buku($slug)
+ {
+  $dataDelete = Book::where('slug', $slug)->first();
+  return view('buku-del', ['dataDelete' => $dataDelete]);
+ }
 
-    public function destroy_buku($slug)
-    {
-        $dataDelete = Book::where('slug', $slug)->first();
-        $dataDelete->delete();
+ public function destroy_buku($slug)
+ {
+  $dataDelete = Book::where('slug', $slug)->first();
+  $dataDelete->delete();
 
-        return redirect('/buku')->with('status', 'Berhasil hapus data : ' . $dataDelete->judul);
-    }
+  return redirect('/buku')->with('status', 'Berhasil hapus data : ' . $dataDelete->judul);
+ }
 
-    public function buku_restore()
-    {
-        $dataRecycle = Book::onlyTrashed()->get();
-        return view('buku-restore', ['data' => $dataRecycle]);
-    }
+ public function buku_restore()
+ {
+  $dataRecycle = Book::onlyTrashed()->get();
+  return view('buku-restore', ['data' => $dataRecycle]);
+ }
 
-    public function restore_buku($slug)
-    {
-        $dataRestore = Book::withTrashed()->where('slug', $slug)->first();
-        $dataRestore->restore();
-        return redirect('/buku')->with('status', 'Berhasil restore data judul : ' . $dataRestore->judul);
-    }
+ public function restore_buku($slug)
+ {
+  $dataRestore = Book::withTrashed()->where('slug', $slug)->first();
+  $dataRestore->restore();
+  return redirect('/buku')->with('status', 'Berhasil restore data judul : ' . $dataRestore->judul);
+ }
 
-    public function fix_deleted_buku($slug)
-    {
-        $dataFix = Book::withTrashed()->where('slug', $slug)->first();
-        File::delete(public_path('storage/upload') . '/' . $dataFix->cover);
-        $dataFix->forceDelete();
-        return redirect('/buku')->with('status', 'Berhasil deleted permanent data : ' . $dataFix->judul);
-    }
+ public function fix_deleted_buku($slug)
+ {
+  $dataFix = Book::withTrashed()->where('slug', $slug)->first();
+  File::delete(public_path('storage/upload') . '/' . $dataFix->cover);
+  $dataFix->forceDelete();
+  return redirect('/buku')->with('status', 'Berhasil deleted permanent data : ' . $dataFix->judul);
+ }
 }
